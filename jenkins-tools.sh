8dd0243a66d937858d53a3bcb951dc9d0a9f14c2
Maxime <maxime.parisi@coopgo.fr>
2025-05-02 16:17:56 +0200

    create : add script to install jenkins installer in docker container

---
+++ jenkins-tools.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker cp "~/Documents/Jenkins-Trinity/jenkins/plugins.txt" "$CONTAINER_NAME":/tmp/plugins.txt
+
+docker exec -u 0 "$CONTAINER_NAME" bash -c "\
+  jenkins-plugin-cli --plugin-file /tmp/plugins.txt --plugins delivery-pipeline-plugin:1.3.2 deployit-plugin && \
+  cp -r -p /usr/share/jenkins/ref/plugins/. /var/jenkins_home/plugins/."
+
