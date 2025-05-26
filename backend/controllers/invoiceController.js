22f3d064bcfb20c3a39ea6c8a9498ee539ce20d5
Maxime <maxime.parisi@coopgo.fr>
2025-05-26 14:23:54 +0200

    fix: add paypal credential

--- backend/controllers/invoiceController.js
+++ backend/controllers/invoiceController.js
@@ -3,8 +3,8 @@ const User = require("../models/user.model");
 
 const paypal = require("@paypal/checkout-server-sdk");
 
-const clientId = process.env.PAYPAL_CLIENT_ID;
-const clientSecret = process.env.PAYPAL_CLIENT_SECRET;
+const clientId = ARs90tAriKVw_sPmOkCFn0jZkl6YONFG-cUsgulvtJ6ydTHZkzl8zMwnMlCMHnYTsv00lyTwH0M0YDDE;
+const clientSecret = ARs90tAriKVw_sPmOkCFn0jZkl6YONFG-cUsgulvtJ6ydTHZkzl8zMwnMlCMHnYTsv00lyTwH0M0YDDE;
 
 const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
 const client = new paypal.core.PayPalHttpClient(environment);
